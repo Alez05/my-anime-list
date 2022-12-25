165a482dbe3378c22cb3f78067a621ccc32b6d78
alez05 <cercel.alexandru436@gmail.com>
2022-12-25 18:12:09 +0000

    feat: add a colection of anime in present of sample animation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,24 @@
+import { useLoaderData } from "@remix-run/react";
+import { siteData } from "~/config/data";
+
+export const loader = () => {
+  const {navigationBar} = siteData
+  return {navigationBar}
+}
+
 const MyAnimeList = () => {
+  const {navigationBar}:any = useLoaderData()
   return (
     <div>
       <h1>titlu</h1>
+      {navigationBar.map((nav:any, key:any) => {
+        return (
+          <div key={key}>
+            <div>{nav.link}</div>
+            <div>{nav.title}</div>
+          </div>
+        )
+      })}
     </div>
   );
 }
